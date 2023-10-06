7a5bdaa07a5450fe1f58639dc05330105f9eb942
kay0829 <eluka97@daum.net>
2023-10-06 17:01:20 +0900

    🔧 1) 절대경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@api", replacement: "/src/api" },
+            { find: "@component", replacement: "/src/common-component" },
+            { find: "@container", replacement: "/src/container" },
+            { find: "@feature", replacement: "/src/feature" },
+            { find: "@type", replacement: "/src/type" },
+            { find: "@hook", replacement: "/src/hook" },
+            { find: "@util", replacement: "/src/util" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
 });
